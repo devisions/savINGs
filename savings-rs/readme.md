66d099656d6f5932742ed57640358f50e11b3678
devisions <marius.ileana@gmail.com>
2019-07-02 01:03:19 +0300

    savings-rs cleanup

--- savings-rs/readme.md
+++
@@ -1,8 +0,0 @@
-# Savings example using Reactive Spring
-
-
-## Usage
-
-Based on the data that is populated at startup, you can:
-- get an account by id using `http://localhost:8080/accounts/savings/1`
-
--- savings-rs/src/main/resources/data-h2.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO users (id, username, password) VALUES (1, 'john', 'doe');
-
-INSERT INTO users (id, username, password) VALUES (2, 'jane', 'doe');
-
-INSERT INTO savings_accounts (id, iban, description, user_id)
-    VALUES (1, 'iban1', 'My savings', 2);
